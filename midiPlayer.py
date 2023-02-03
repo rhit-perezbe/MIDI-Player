33beeb2b381b6743323753ccb9f0880a54c27bbf
rhit-perezbe <perezbe@rose-hulman.edu>
2023-02-03 22:00:32 +0000

    Updated Python File

--- midiPlayer.py
+++ midiPlayer.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python3
 import os
-print("test")
-soundfont = 'sf2files/8bitsf.SF2'
-sound = 'midis/ruby.mid'
-whole_command = 'fluidsynth -a alsa -m alsa_seq -i ' + soundfont + ' ' + sound
-os.system(whole_command)
-print("midi done")
+import random
+while True:
+    print(os.listdir('./midis'))
+    mumsic = os.listdir('./midis')
+    soundfont = 'sf2files/Sonic_And_Knuckles_-Updated-.sf2'
+    size = len(mumsic)
+    sound = mumsic[random.randint(0, size - 1)]
+    whole_command = 'fluidsynth -a alsa -m alsa_seq -i ' + soundfont + ' midis/' + 'ruby4.mid'
+    print(sound)
+    os.system(whole_command)
+    print("midi done")
+    break
 
