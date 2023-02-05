1565165d764f76399a585ea8d34a98bfb752538d
rhit-perezbe <perezbe@rose-hulman.edu>
2023-02-05 21:17:05 +0000

    Added killing process

--- midiPlayer.py
+++ midiPlayer.py
@@ -1,15 +1,29 @@
 #! /usr/bin/env python3
 import os
 import random
+import subprocess
+import time
+nums = 0
 while True:
-    print(os.listdir('./midis'))
+    #print(os.listdir('./midis'))
     mumsic = os.listdir('./midis')
-    soundfont = 'sf2files/Sonic_And_Knuckles_-Updated-.sf2'
+    soundfont = 'sf2files/8bitsf.SF2'
     size = len(mumsic)
     sound = mumsic[random.randint(0, size - 1)]
-    whole_command = 'fluidsynth -a alsa -m alsa_seq -i ' + soundfont + ' midis/' + 'ruby4.mid'
-    print(sound)
+    whole_command = 'fluidsynth -a alsa -m alsa_seq -i ' + soundfont + ' midis/' + 'crab.mid'  + '&'
+    #print(sound)
     os.system(whole_command)
+    processes = subprocess.check_output("ps", shell = True)
+    print(processes)
+    print(type(processes))
+    procString = str(processes, 'utf-8')
+    procName = procString.find('fluidsynth')
+    print(procString[(procName - 23):(procName - 19)])
+    time.sleep(5)
+    #os.system('kill ' + procString[(procName - 23):(procName -19)])
     print("midi done")
-    break
+    time.sleep(2)
+    nums = nums + 1
+    if nums > 0:
+        break
 
