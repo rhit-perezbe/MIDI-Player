61b369bd458b1f13a49acf2f2fd6c6dc3cf2bce3
rhit-perezbe <perezbe@rose-hulman.edu>
2023-02-08 06:08:44 +0000

    added button reading

--- midiPlayer.py
+++ midiPlayer.py
@@ -3,27 +3,51 @@ import os
 import random
 import subprocess
 import time
+import gpiod
+import sys
 nums = 0
+
+os.system('./setup.sh')
+
+CONSUMER = 'getset'
+CHIP = '1'
+getoffsets = [28,18,16,17]
+setoffsets = [13,12,15,14]
+chip = gpiod.Chip(CHIP)
+getlines = chip.get_lines(getoffsets)
+getlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_FALLING_EDGE)
+setlines = chip.get_lines(setoffsets)
+setlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
+
+print("written")
+
 while True:
-    #print(os.listdir('./midis'))
     mumsic = os.listdir('./midis')
-    soundfont = 'sf2files/8bitsf.SF2'
+    soundfont = 'sf2files/hgss.sf2'
     size = len(mumsic)
     sound = mumsic[random.randint(0, size - 1)]
-    whole_command = 'fluidsynth -a alsa -m alsa_seq -i ' + soundfont + ' midis/' + 'crab.mid'  + '&'
-    #print(sound)
+    whole_command = 'fluidsynth -a alsa -m alsa_seq -i ' + soundfont + ' midis/' + 'meteor.mid'  + '&'
     os.system(whole_command)
+    
     processes = subprocess.check_output("ps", shell = True)
-    print(processes)
-    print(type(processes))
     procString = str(processes, 'utf-8')
     procName = procString.find('fluidsynth')
-    print(procString[(procName - 23):(procName - 19)])
+    pid = procString[(procName - 23):(procName - 19)]
+
     time.sleep(5)
-    #os.system('kill ' + procString[(procName - 23):(procName -19)])
-    print("midi done")
+    #os.system('kill ' + pid)
     time.sleep(2)
+    
     nums = nums + 1
     if nums > 0:
         break
+print("closing")
+while True:
+    ev_lines = getlines.event_wait(sec=1)
+    vals = getlines.get_values()
+    vals[2] = vals[2] ^ 1
+    print(vals)
+    setlines.set_values(vals)
+    time.sleep(0.1)
+
 
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+config-pin P8_11 gpio
+config-pin P8_12 gpio
+config-pin P8_15 gpio
+config-pin P8_16 gpio
+config-pin P9_12 gpio
+config-pin P9_14 gpio
+config-pin P9_15 gpio
+config-pin P9_23 gpio
